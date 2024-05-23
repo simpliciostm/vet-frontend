1dcc8d380485b7320dae3ca19c408d7adbe525d3
Simplicio.dev <dev06@consulting.allog.com.br>
2024-05-22 23:55:45 -0300

    feat(pageUser): user page validations and pagination

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -6,7 +6,6 @@ import { setStorage } from '../../services/localStorage';
 import './style.css'
 import { Button, TextField } from '@mui/material';
 import img from '../../assets/images/logo.jpeg'
-import { TextFieldInputComponent } from '../../components/inputComponent';
 
 export const Login = () => {
     const [email, setEmail] = useState('');
@@ -14,37 +13,43 @@ export const Login = () => {
     const [alert, setAlert] = useState(false);
     const [status, setStatus] = useState('');
     const [msg, setMsg] = useState('');
-    const [showPassword, setShowPassword] = useState(true);  
-
-    const getChange = (email: string) => {
-        setEmail(email);
-    }
+    const [showPassword, setShowPassword] = useState(true);
+    const [inputErrorEmail, setInputErrorEmail] = useState(false);
+    const [inputErrorPassword, setInputErrorPassword] = useState(false);
 
     const getPassword = (password: string) => {
         setPassword(password);
     }
 
+    const getEmail = (email: string) => {
+        setEmail(email);
+    }
+
     const login = async (e: any) => {
         e.preventDefault();
 
-        const response = await api.post('/login', {
-            email, password
-        });
+        const isValidateErrors = validateErrors();
 
-        if (response.data.status === 0) {
-            setAlert(true);
-            setStatus('error');
-            setMsg(response.data.msg);
-            timer();
-        } else if (response.data.status === 1) {
-            setAlert(true);
-            setStatus('success');
-            setMsg(response.data.msg);
-            timer();
-            setStorage('token', response.data.token);
-            setStorage('id', response.data.idUser);
-            setStorage('auth', response.data.auth);
-            window.location.href = '/dashboard/home';
+        if (!isValidateErrors) {
+            const response = await api.post('/login', {
+                email, password
+            });
+
+            if (response.data.status === 0) {
+                setAlert(true);
+                setStatus('error');
+                setMsg(response.data.msg);
+                timer();
+            } else if (response.data.status === 1) {
+                setAlert(true);
+                setStatus('success');
+                setMsg(response.data.msg);
+                timer();
+                setStorage('token', response.data.token);
+                setStorage('id', response.data.idUser);
+                setStorage('auth', response.data.auth);
+                window.location.href = '/dashboard/home';
+            }
         }
     }
 
@@ -54,29 +59,37 @@ export const Login = () => {
         }, 3000)
     }
 
+    const validateErrors = (): boolean => {
+        email ? setInputErrorEmail(false) : setInputErrorEmail(true);
+        password ? setInputErrorPassword(false) : setInputErrorPassword(true);
+
+        if (inputErrorEmail || inputErrorPassword) return true
+        else return false
+    }
+
     return (
         <div className='background-login'>
             <div className='modal'>
-            <div className='box-img'>
-                <img src={img} alt="logo" />
-            </div>
-            <form>
-                <div className="box-input">
-                    <TextFieldInputComponent label='Email' type='email' onChange={(value: string) => getChange(value)} />
-                    <div className='fields'>
-                        <TextField className='text-field' onChange={(e) => getPassword(e.target.value)} id="outlined-basic" label="Password" variant="outlined" type={showPassword ? "password" : "text"} size='small' />
-                        {showPassword ? (
-                            <i onClick={() => setShowPassword(false)} id='eye' className="bi bi-eye-fill"></i>
-                        ) : (
-                            <i onClick={() => setShowPassword(true)} id='eye' className="bi bi-eye-slash-fill"></i>
-                        )}
+                <div className='box-img'>
+                    <img src={img} alt="logo" />
+                </div>
+                <form>
+                    <div className="box-input">
+                        <TextField error={inputErrorEmail} focused={inputErrorEmail} className='text-field' onChange={(e) => getEmail(e.target.value)} id="outlined-basic" label="Email" variant="outlined" type='email' size='small' />
+                        <div className='fields'>
+                            <TextField error={inputErrorPassword} focused={inputErrorPassword} className='text-field' onChange={(e) => getPassword(e.target.value)} id="outlined-basic" label="Password" variant="outlined" type={showPassword ? "password" : "text"} size='small' />
+                            {showPassword ? (
+                                <i onClick={() => setShowPassword(false)} id='eye' className="bi bi-eye-fill"></i>
+                            ) : (
+                                <i onClick={() => setShowPassword(true)} id='eye' className="bi bi-eye-slash-fill"></i>
+                            )}
+                        </div>
                     </div>
-                </div>
-                <div className='button-fields'>
-                    <Button className='button' onClick={(e) => login(e)} sx={{ width: '200px' }} variant='contained' >Entrar</Button>
-                </div>
-            </form>
-        </div>
+                    <div className='button-fields'>
+                        <Button className='button' onClick={(e) => login(e)} sx={{ width: '200px' }} variant='contained' >Entrar</Button>
+                    </div>
+                </form>
+            </div>
             {alert ? <ShowAlert status={status} msg={msg} /> : null}
         </div>
     )
--- src/pages/usuario/index.tsx
+++ src/pages/usuario/index.tsx
@@ -3,39 +3,102 @@ import { Header } from '../../components/headerComponent';
 import { TableUsersComponent } from '../../components/tableUsersComponent'
 import { Button, TextField, Typography } from '@mui/material';
 import './style.css'
-import SearchIcon from '@mui/icons-material/Search';
 import AddIcon from '@mui/icons-material/Add';
 import { Link } from 'react-router-dom';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import api from '../../api';
 import { UserFormComponent } from '../../components/userFormComponent';
+import SearchIcon from '@mui/icons-material/Search';
+import ClearIcon from '@mui/icons-material/Clear';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+interface filterProps {
+    name: string,
+    email: string
+}
 
 export const Usuarios = () => {
     const [users, setUsers] = useState([]);
     const [columns, setColumns] = useState([]);
     const [showAdd, setShowAdd] = useState(false);
     const [operation, setOperation] = useState('');
+    const [emailFilter, setEmailFilter] = useState('');
+    const [nameFilter, setNameFilter] = useState('');
+    const [currentPage, setCurrentPagination] = useState(0);
+    const [totalUsers, setTotalUsers] = useState(0);
+    const [filterUser, setFilterUser] = useState<filterProps>({ name: '', email: '' });
+    const [totalPageLastClick, setTotalPageLastClick] = useState(0);
+    const [disableButtonNext, setDisableButtonNext] = useState(false);
+    const [disableButtonBack, setDisableButtonBack] = useState(false);
+    const [msg, setMsg] = useState('');
 
     useEffect(() => {
-        const getUsers = async () => {
-            try {
-                const { data } = await api.get(`/userList`);
+        getUsers(filterUser, currentPage);
+        const pagesNumbers = Math.ceil(totalUsers / 5);
+        if (pagesNumbers - 1 === totalPageLastClick) setDisableButtonNext(true);
+        if (totalPageLastClick === 0) setDisableButtonBack(true);
 
-                if (data && data.status === 1) {
-                    setUsers(data.data)
-                    setColumns(data.columns)
-                }
-            } catch (err) {
-                console.log(err);
+    }, [users, filterUser, totalPageLastClick, totalUsers, currentPage]);
+
+    const getUsers = async (filter: filterProps, currentPage: number) => {
+        try {
+            const { data } = await api.post(`/userList/${currentPage}/${5}`, {
+                filter
+            });
+
+            switch (data.status) {
+                case 1:
+                    setUsers(data.data);
+                    setTotalUsers(data.total);
+                    setColumns(data.columns);
+                    break;
+
+                case 0:
+                    setUsers([]);
+                    setMsg(data.msg);
+                    break;
             }
-        }
 
-        getUsers();
-    }, [users]);
+        } catch (err) {
+            console.log(err);
+        }
+    }
 
     const openAdd = () => {
         !showAdd ? setShowAdd(true) : setShowAdd(false);
-        setOperation('register')
+        setOperation('register');
+    }
+
+    const clearFilter = async (e: any) => {
+        e.preventDefault();
+        setFilterUser({
+            name: '',
+            email: ''
+        });
+        setNameFilter('');
+        setEmailFilter('');
+    }
+
+    const applyFilter = async (e: any) => {
+        e.preventDefault();
+        setFilterUser({
+            name: nameFilter,
+            email: emailFilter
+        });
+    }
+ 
+    const nextPagination = async () => {
+        setTotalPageLastClick(totalPageLastClick + 1);
+        setCurrentPagination(currentPage + 5);
+        await getUsers(filterUser ,currentPage)
+        setDisableButtonBack(false);
+    }
+
+    const backPagination = async () => {
+        setTotalPageLastClick(totalPageLastClick - 1);
+        setCurrentPagination(currentPage - 5);
+        await getUsers(filterUser ,currentPage)
+        setDisableButtonNext(false)
     }
 
     return (
@@ -57,25 +120,39 @@ export const Usuarios = () => {
                         <AddIcon />
                     </div>
                 </div>
-                <div className="data-null">
-                    {users.length >= 1 ? (
-                        <div className='box-data'>
-                            <div className="box-filter">
-                                <div className='fields'>
-                                    <div className="fields-filter">
-                                        <TextField color='success' style={{ marginLeft: 20 }} id="standard-basic" label="Nome" variant="standard" size='small' />
-                                        <TextField color='success' id="standard-basic" label="Email" variant="standard" size='small' />
+                <div className='box-data'>
+                    <div className="box-filter-users">
+                        <div className='fields-users'>
+                            <div className="fields-filter">
+                                <TextField focused={false} value={nameFilter} onChange={(e) => setNameFilter(e.target.value)} color='primary' style={{ marginLeft: 20 }} id="outlined-basic" label="Nome" variant="outlined" size='small' />
+                                <TextField focused={false} value={emailFilter} onChange={(e) => setEmailFilter(e.target.value)} color='primary' id="outlined-basic" label="Email" variant="outlined" size='small' />
+                            </div>
+                            <div className="search-button-users">
+                                <Button size='small' onClick={(e) => applyFilter(e)} className='button-filter-users' variant="contained" endIcon={<SearchIcon />} >Filtrar</Button>
+                                <Button size='small' onClick={(e) => clearFilter(e)} className='button-remove-filter-users' variant="contained" ><ClearIcon fontSize='small' /></Button>
+                            </div>
+                        </div>
+                    </div>
+                    {
+                        users.length >= 1 ? (
+                            <>
+                                <TableUsersComponent data={users} columns={columns} />
+                                <div className="table-users-pagination">
+                                    <div className="table-users-quantity">
+                                        <Typography component={'span'} fontFamily={'sans-serif'}>Qtd: {totalUsers}</Typography>
                                     </div>
-                                    <div className="search-button">
-                                        <Button className='button' variant="contained" endIcon={<SearchIcon />} >Filtrar</Button>
+                                    <div className="table-users-button">
+                                        <button className={disableButtonBack ? 'button-pagination-user-disabled' : 'button-pagination-user'} disabled={disableButtonBack} onClick={() => backPagination()} ><KeyboardArrowLeftIcon /></button>
+                                        <button className={disableButtonNext ? 'button-pagination-user-disabled' : 'button-pagination-user'} disabled={disableButtonNext} onClick={() => nextPagination()}><KeyboardArrowRightIcon /></button>
                                     </div>
                                 </div>
+                            </>
+                        ) : (
+                            <div>
+                                <Typography>{msg}</Typography>
                             </div>
-                            <TableUsersComponent data={users} columns={columns} />
-                        </div>
-                    ) : (
-                        <Typography component={'span'}>Não existe usuários cadastrados</Typography>
-                    )}
+                        )
+                    }
                 </div>
                 {showAdd ? <UserFormComponent operation={operation} /> : null}
             </div>
